8439ede1da2d8ee3b309e8bca58421bf02a79701
liubasara <followliu@163.com>
2019-09-27 18:06:26 +0800

    实战开发环境配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,5 +1,25 @@
-const merge = require("webpack-merge")
+const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 
 module.exports = merge(common, {
+  //详情https://www.webpackjs.com/configuration/devtool/
+  devtool: 'inline-cheap-module-source-map',
+  devServer: {
+    contentBase: 'index.html',
+    port: 8080,
+    host: 'localhost',
+    overlay: true,
+    compress: true,
+    open: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    //启用热更新配置项
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
 })
